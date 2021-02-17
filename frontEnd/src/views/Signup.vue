4a2eb4ca4b72655142cc38f2762fc6b0ec482e7a
velvetarchangel <h.dastidar@gmail.com>
2021-02-16 21:36:17 -0700

    Fixed long email signup issue

--- frontEnd/src/views/Signup.vue
+++ frontEnd/src/views/Signup.vue
@@ -130,7 +130,8 @@ export default {
 
         emailrules: [ 
         v => 
-        !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) 
+        !v 
+        || /.+@.+\..+/.test(v) 
         || 'E-mail must be valid'
         || 'E-mail is required'
         ],
